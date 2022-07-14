4a276019091acd16283e552f5540f96bb7b3bdd3
Syed <syedtathahiruddin@gmail.com>
2022-07-14 21:28:17 +0530

    second commit

--- remainder.c
+++ remainder.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 
 int main (){
-    
-    
+    /*
+    int a = (int)1.999999;
+    printf("%d\n",a);
+
+    // operator precedence
+    //*,/,%
+    //+,-
+    //= 'assignment'
+    */
+    printf("%f\n", 4 / 2.0);
+    printf("%d\n", 2 / 3);
+    printf("%f \n", 3.0 / 2);
 
 
 
