8433f29fbd4a2d9fc0a87ab913071d938222d741
Syed <syedtathahiruddin@gmail.com>
2022-07-14 21:09:01 +0530

    added airthmatic instructions

--- instructions&operators.c
+++ instructions&operators.c
@@ -1,7 +1,33 @@
 # include<stdio.h>
 
 int main(){
+    /*
+    int a = 22;
+    int b = a;
+    int c = b * 6;
+    int d = 1,e;
+
+    int years = 2;
+    int oldAge = 22;
+    int newAge = oldAge + years;
     
+    int x,y,z;
+    x = y = z = 4;
+    
+// airthmetic instructions
+    int a = 1, b = 2;
+    int sum = a + b;
+    int multiply = a * b;
+    int x,y = a * b ;
+    */
+
+    int b , c ;
+    b = c = 1;
+    int a = b + c;
+
+    printf( "a :%d ",&a);
+
+
 
     return 0;
 }
