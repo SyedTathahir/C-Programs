e1b9387874a803839e5711b73719c65b1b6f2ddc
Syed <syedtathahiruddin@gmail.com>
2022-07-14 21:32:09 +0530

    third commit

--- remainder.c
+++ remainder.c
@@ -10,10 +10,13 @@ int main (){
     //*,/,%
     //+,-
     //= 'assignment'
-    */
+    
     printf("%f\n", 4 / 2.0);
     printf("%d\n", 2 / 3);
     printf("%f \n", 3.0 / 2);
+    */
+    int a =(int) 1.999999;
+    printf("%d \n", a);
 
 
 
