ff9d9d604494f96d8cfde5f172ef79cbbcd018f8
Syed <syedtathahiruddin@gmail.com>
2022-07-16 18:53:36 +0530

    added one question

--- conditions.c
+++ conditions.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 
 int main(){
+
     int age ;
     printf("Enter age : ");
     scanf("%d",&age);
 
-    if()
-
-
+    if(age > 18){
+        printf("\nAdult");
+        printf("\nThey can Vote");
+        printf("\nThey can Drive");
+    }
+    else{
+        printf("\nNot Adult");
+    }
+    printf("\nThank You");
 
     return 0;
 }
