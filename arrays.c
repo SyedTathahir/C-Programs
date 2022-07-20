26069d875439deccd532ee2986d3e0e9fbb41be6
Syed <syedtathahiruddin@gmail.com>
2022-07-20 16:53:35 +0530

    Explained 1d arrays

--- arrays.c
+++ arrays.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 
 int main(){
+    /*
     //ARRAYS:
-    
+    int luckyNumber[6] = {4, 8, 15, 16, 23, 42};
+    //           indexes- 0  1  2   3   4   5 
+    //modify arrays:
+    luckyNumber[0] = 60;
+    printf("%d \n",luckyNumber[0]);
+    printf("%d \n",luckyNumber[1]);
+    */
 
-
+   
 
     return 0;
 }
