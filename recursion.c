6a97f33ff746002e6fcd0106b59818b25a5d9c87
Syed <syedtathahiruddin@gmail.com>
2022-07-28 19:54:39 +0530

    Added question 31

--- recursion.c
+++ recursion.c
@@ -19,4 +19,38 @@ void printHW(int count){
     printf("Hello World\n");
     printHW(count- 1);
 }
+
+//Recursion maths:
+  f(x) = x^2
+  f(f(f(x))) for x = 2
+  f(2) = 2^2 = 4
+  f(4) = 4^2 = 16
+  f(16) = 16 ^ 2 = 256
+//Normal function call:
+function call another function and do task and return
+to main function.
+//Recursion function call:
+it calls itself.
+// base case.
+
 */
+
+//Q.sum of n natural numbers?
+
+int  sum(int n );
+
+int main (){
+    printf("Sum is : %d ",sum (5));
+
+    return 0;
+}
+//recursive function
+
+int sum(int n){
+    if (n==1){
+        return 1;
+    }
+    int sumNm1 = sum (n-1); // sum of 1 to n
+    int sumN = sumNm1 + n;
+    return sumN;
+}
